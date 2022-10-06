86d297a96d6fd1ccdbb9679eb6ad763f6f4d2db0
Pedro Aires <pedro.cabral@ccc.ufcg.edu.br>
2022-10-06 17:50:59 -0300

    projeto vazio com dependencia do spring web adicionado

---
+++ chamarDeSeu/src/main/java/poi/atvdd1/chamarDeSeu/ChamarDeSeuApplication.java
@@ -0,0 +1,13 @@
+package poi.atvdd1.chamarDeSeu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamarDeSeuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChamarDeSeuApplication.class, args);
+	}
+
+}
